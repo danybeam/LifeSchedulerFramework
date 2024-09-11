76f5323cf6dab74778387c301f406fb6da688553
Daniel Orozco <danybeam@gmail.com>
2024-09-11 16:33:10 -0400

    add barebones things

---
+++ libLifeSchedulerFramework/LifeSchedulerFramework.cpp
@@ -0,0 +1,10 @@
+﻿// LifeSchedulerFramework.cpp : Defines the entry point for the application.
+//
+
+#include <libLifeSchedulerFramework/LifeSchedulerFramework.h>
+#include <iostream>
+
+void doExternalThing()
+{
+	std::cout << "Hello CMake." << std::endl;
+}
